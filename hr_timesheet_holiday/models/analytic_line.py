2ddd4523b9a5189ed63f2af4f451cc6cb7822cbb
AaronHForgeFlow <ahenriquez@eficent.com>
2022-12-14 09:03:48 +0100

    [IMP] hr_timesheet_holiday: allow editing timesheet for holidays, remove constraint

--- hr_timesheet_holiday/models/analytic_line.py
+++ hr_timesheet_holiday/models/analytic_line.py
@@ -31,15 +31,3 @@ class AnalyticLine(models.Model):
                 if sheets:
                     ts_line.sheet_id_computed = sheets[0]
                     ts_line.sheet_id = sheets[0]
-
-    @api.multi
-    def write(self, vals):
-        if not self.env.context.get('force_write', False):
-            for rec in self:
-                if rec.account_id.is_leave_account \
-                        and rec.leave_id:
-                    raise ValidationError(_(
-                        "This line is protected against editing because it "
-                        "was created automatically by a leave request. "
-                        "Please edit the leave request instead."))
-        super(AnalyticLine, self).write(vals)
